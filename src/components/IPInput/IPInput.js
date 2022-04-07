d0c255832fada4e43c80f2e3ba7f002ba82712da
linnaek <linnaekraemer@gmail.com>
2022-04-06 23:18:12 -0500

    move validation pattern to parent component in order to display error

--- src/components/IPInput/IPInput.js
+++ src/components/IPInput/IPInput.js
@@ -1,7 +1,6 @@
 import styles from './IPInput.module.css'
 
 function IPInput (props) {
-  console.log('IPINPUT value', props.value)
   return (
     <div>
       <input 
@@ -9,7 +8,6 @@ function IPInput (props) {
         minLength="7" 
         maxLength="18" 
         size="15" 
-        pattern="\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))(\/(([3]?[0-2])|([0-2]?[0-9])))?\b"
         value={props.value}
         onChange={props.onChange}
         className={styles.ipInput}
